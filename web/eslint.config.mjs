29ef0fe354f82c53a9f77f73d5316f684dcab2c8
xiaotang <taozi@h5.com>
2025-08-29 16:54:29 +0800

    🔧 eslint配置变更

--- web/eslint.config.mjs
+++ web/eslint.config.mjs
@@ -1,11 +1,23 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from "./.nuxt/eslint.config.mjs";
+import tsParser from "@typescript-eslint/parser";
 
 export default withNuxt(
   // Your custom configs here
-    {
+  {
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        project: {
+          extends: "./tsconfig.json",
+          include: ["app/**/*.ts", "app/**/*.vue", "nuxt.config.ts"],
+        },
+      },
+    },
     rules: {
-      'vue/multi-word-component-names': 'off'
-    }
+      "vue/multi-word-component-names": "off",
+    },
   }
-)
+);
