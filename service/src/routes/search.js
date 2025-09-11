2c32a6201cfdeeff79e4567eb29e7cd99642f88f
xiaotang <taozi@h5.com>
2025-09-11 10:41:55 +0800

    ✨ 添加随机单词初版

--- service/src/routes/search.js
+++ service/src/routes/search.js
@@ -1,6 +1,7 @@
 const Router = require("koa-router");
 const { searchWord } = require("../services/search");
 const { translate } = require("../services/translate");
+const { randomWord } = require("../services/search");
 
 const router = new Router();
 
@@ -9,5 +10,8 @@ router.get("/word", searchWord);
 
 // 翻译
 router.get("/translate", translate);
+
+// 获取随机单词
+router.get("/random", randomWord);
 
 module.exports = router;
--- service/src/services/search.js
+++ service/src/services/search.js
@@ -1,7 +1,7 @@
 const { wordSchema } = require("../validators/search.validator");
-const Stardict = require("../models/stardict");
-const BaiduiSearch = require("../utils/baiduSearch");
+const Ecdict = require("../models/ecdict");
 
+// 单词查询接口
 const searchWord = async (ctx) => {
   try {
     const { word } = ctx.request.query;
@@ -26,6 +26,29 @@ const searchWord = async (ctx) => {
     ctx.throw(200, error.message);
   }
 };
+
+// 随机查询单词
+const randomWord = async (ctx) => {
+  try {
+    const wordInfo = await Ecdict.findAll({
+      order: Ecdict.sequelize.random(),
+      limit: 10,
+    });
+    if (!wordInfo) {
+      return ctx.throw(200, "查询失败");
+    }
+    return (ctx.body = {
+      code: 0,
+      message: "查询成功",
+      data: {
+        wordInfo: wordInfo,
+      },
+    });
+  } catch (error) {
+    ctx.throw(200, error.message);
+  }
+};
 module.exports = {
   searchWord,
+  randomWord
 };
